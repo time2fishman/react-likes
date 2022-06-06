646519cfa85e24551ecd69778cc745ba3d891547
esin87 <esin.saribudak@gmail.com>
2022-06-05 21:40:53 -0500

    Update dependency versions

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+	return (
+		<div className='App'>
+			<h1>Hello World</h1>
+			<h2>Time to start coding ⚛</h2>
+		</div>
+	);
 }
 
 export default App;
