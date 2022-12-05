18482415d188e9cd86efb0d4559fbb0a559e63bb
time2fishman <adam_h_martinez@hotmail.com>
2022-12-05 16:35:58 -0700

    add anothercounter by 3s

--- src/components/LikeTotal.js
+++ src/components/LikeTotal.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
 const LikeTotal = (props) => {
-    
+
     return (
-        <div className='counter-container'>
-            <button className='decrement-button' onClick={() => props.setTotalLikes(Math.max(0, props.totalLikes - 1))}>
-                -
-            </button>
-            <p className='total-likes'>{props.totalLikes}</p>
-            <button className='increment-button' onClick={() => props.setTotalLikes(props.totalLikes + 1)}>
-                +
-            </button>
+        <div>
+            <div className='counter-container'>
+                <button className='decrement-button' onClick={() => props.setTotalLikes(Math.max(0, props.totalLikes - 1))}>
+                    -
+                </button>
+                <p className='total-likes'>{props.totalLikes}</p>
+                <button className='increment-button' onClick={() => props.setTotalLikes(props.totalLikes + 1)}>
+                    +
+                </button>
+            </div>
+            <div className='counter-container'>
+                <button className='decrement-button' onClick={() => props.setTotalLikes3(Math.max(0, props.totalLikes3 - 3))}>
+                    -
+                </button>
+                <p className='total-likes'>{props.totalLikes3}</p>
+                <button className='increment-button' onClick={() => props.setTotalLikes3(props.totalLikes3 + 3)}>
+                    +
+                </button>
+            </div>
         </div>
     );
 };
--- src/components/Likes.js
+++ src/components/Likes.js
@@ -3,12 +3,12 @@ import LikeTotal from './LikeTotal';
 
 const Likes = () => {
     const [totalLikes, setTotalLikes] = useState(0)
-    // console.log(totalLikes);
+    const [totalLikes3, setTotalLikes3] = useState(0)
 
     return (
-        // <div>
-            <LikeTotal totalLikes={totalLikes} setTotalLikes={setTotalLikes} />
-        // </div>
+        <div>
+            <LikeTotal totalLikes={totalLikes} setTotalLikes={setTotalLikes} totalLikes3={totalLikes3} setTotalLikes3={setTotalLikes3} />
+        </div>
     );
 };
 
